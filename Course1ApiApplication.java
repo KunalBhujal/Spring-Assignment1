b1c93a2758b43ccb6a14576b20b3c9a47c49ef97
KunalBhujal <kunalb@heaptrace.net>
2023-03-11 16:52:06 +0530

    This project for practice

---
+++ Course1ApiApplication.java
@@ -0,0 +1,13 @@
+package io.javaht.springbootstart.course1api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1ApiApplication.class, args);
+	}
+
+}
